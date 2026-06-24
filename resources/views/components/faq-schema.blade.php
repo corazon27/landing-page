0df05bdc0ba62e8819479e97e7efaade670be394
Muhammad Alfian <62877827+corazon27@users.noreply.github.com>
2026-06-24 17:50:10 +0700

    Refactor FAQ schema generation in Blade template

--- resources/views/components/faq-schema.blade.php
+++ resources/views/components/faq-schema.blade.php
@@ -3,24 +3,22 @@
 @if(!empty($faqs))
 @php
 $schemaItems = collect($faqs)->map(fn($faq) => [
-'@type' => 'Question',
-'name' => $faq['question'],
-'acceptedAnswer' => [
-'@type' => 'Answer',
-'text' => strip_tags($faq['answer']),
-],
+    '@type' => 'Question',
+    'name' => $faq['question'],
+    'acceptedAnswer' => [
+        '@type' => 'Answer',
+        'text' => strip_tags($faq['answer']),
+    ],
 ])->values()->all();
 
 $faqSchema = [
-'@context' => 'https://schema.org',
-'@type' => 'FAQPage',
-'mainEntity' => $schemaItems,
+    '@context' => 'https://schema.org',
+    '@type' => 'FAQPage',
+    'mainEntity' => $schemaItems,
 ];
 @endphp
 
 <script type="application/ld+json">
-{
-    !!json_encode($faqSchema, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) !!
-}
+{!! json_encode($faqSchema, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) !!}
 </script>
 @endif
